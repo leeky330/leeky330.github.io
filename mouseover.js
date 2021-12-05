a40f02d9e5057523cca84cc5a0e2e9aaf674cd8e
leeky330 <92727096+leeky330@users.noreply.github.com>
2021-12-05 10:04:45 +0900

    Update mouseover.js

--- mouseover.js
+++ mouseover.js
@@ -1,2 +1,2 @@
 function over(obj) {obj.scr="https://user-images.githubusercontent.com/92727096/144729179-7bf50d22-2eaa-42f5-8575-fa38add3077b.png";}
-function over(obj) {obj.scr="https://user-images.githubusercontent.com/92727096/144701962-d3d9ae76-b98b-461d-8e7e-da6dc33187c9.jpg";}
+function out(obj) {obj.scr="https://user-images.githubusercontent.com/92727096/144701962-d3d9ae76-b98b-461d-8e7e-da6dc33187c9.jpg";}
